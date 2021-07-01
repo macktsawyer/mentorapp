25d6be7b768add158b97a8f8364e5ebfec88b386
macktsawyer <macktsawyer@yahoo.com>
2021-07-01 00:13:34 -0400

    Login Form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Form, Button, Card } from 'react-bootstrap'
 
 
 const Login = () => {
     return (
         <div>
-            
+            <Card>
+                <Card.body>
+                    <Card.group>
+                        <Form>
+                            
+                        </Form>
+                    </Card.group>
+                </Card.body>
+            </Card>
         </div>
     )
 }
