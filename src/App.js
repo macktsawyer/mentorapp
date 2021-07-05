e63e7f4f38c21baf623fc252167c78faabea2389
macktsawyer <macktsawyer@yahoo.com>
2021-07-04 23:06:05 -0400

    Big fancy changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.scss';
-import Login from './components/Login'
 import Signup from './components/Signup'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
-    <div>
-      <Signup />
-    </div>
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px"}}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 
