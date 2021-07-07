ad0ee29d6dbbf610dd3f49f16dbbc232a15bd4af
macktsawyer <macktsawyer@yahoo.com>
2021-07-07 00:47:44 -0400

    More changes to signup and login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
-import { useAuth } from '../contexts/AuthContext'
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
     const emailRef = useRef();
@@ -43,7 +44,7 @@ export default function Login() {
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? Log In
+                Need an account? <Link to="/Signup">Sign up</Link>
             </div>
         </div>
     )
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
-import { useAuth } from '../contexts/AuthContext'
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
+import { Link } from 'react-router-dom';
 
 export default function Signup() {
     const emailRef = useRef();
@@ -50,7 +51,7 @@ export default function Signup() {
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? Log In
+                Already have an account? <Link to="/Login">Log In</Link>
             </div>
         </div>
     )
