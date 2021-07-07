b68c9817baf173a84c3bb42d5ee781f1dcb705e8
macktsawyer <macktsawyer@yahoo.com>
2021-07-06 21:29:30 -0400

    Working on login and dashboard

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import '../App.scss';
+import Signup from './Signup';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Login from './Login';
+
+function App() {
+  return (
+
+      <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px"}}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/Signup" component={Signup} />
+                <Route path="/Login" component={Login} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+  );
+}
+
+export default App;
