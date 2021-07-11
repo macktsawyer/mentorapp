666d6dd34be612cb72944dd92be6f992ae74a1ed
macktsawyer <macktsawyer@yahoo.com>
2021-07-11 00:44:40 -0400

    Working on front page

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
+import { Button, Card, Nav, Navbar } from 'react-bootstrap';
 
 export default function FrontPage() {
     return (
         <div>
+            <Navbar fixed="top" variant="pills" className="justify-content-end">
+                <Nav.Item>
+                    <Button href="/Login" variant="outline-info" to="/Login">Login</Button>
+                </Nav.Item>
+                <Nav.Item>
+                    <Button href="/Signup" variant="outline-info" size="sm" className="ms-2 me-3" to="/Signup">Sign Up</Button>
+                </Nav.Item>
+            </Navbar>
             <Card>
                 <Card.Body>
-                <Button className="w-100 text-center mt-2">Get Started</Button>
+                    Welcome lets get start this is just filler text for now but I am like super glad you are here like heck yeah lets
+                    do this buddy
+                    <Button href="/Signup" className="w-100 text-center mt-2">Get Started</Button>
                 </Card.Body>
             </Card>
         </div>
