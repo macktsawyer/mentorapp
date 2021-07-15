c8eca5b38082163794d74334ad728a4694e1f5c6
macktsawyer <macktsawyer@yahoo.com>
2021-07-15 00:38:47 -0400

    More changes, still learning

--- src/components/LanguageForm.js
+++ src/components/LanguageForm.js
@@ -5,18 +5,18 @@ export default function LanguageForm() {
     const [languages, setLanguages] = useState();
 
     function handleLanguageChange(e) {
-        e.preventDefault();
-
+        this.setLanguage(e.target.value)
     }
+    
     return (
         <div>
             <Form>
                 <Form.Group id="language-group" className="mt-3">
                     <Form.Label>Programming Languages:</Form.Label>
-                    <Form.Check type="checkbox" label="Javascript" />
-                    <Form.Check type="checkbox" label="Python" />
-                    <Form.Check type="checkbox" label="Ruby" />
-                    <Form.Check type="checkbox" label="Java" />
+                    <Form.Check value="javascript" type="checkbox" label="Javascript" />
+                    <Form.Check value="python" type="checkbox" label="Python" />
+                    <Form.Check value="ruby" type="checkbox" label="Ruby" />
+                    <Form.Check value="java" type="checkbox" label="Java" />
                     <Button type="submit">Submit</Button>
                 </Form.Group>
             </Form>
