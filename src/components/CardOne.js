9c69aed796363e1dc8339e93073addad6aa066ae
macktsawyer <macktsawyer@yahoo.com>
2021-08-08 00:38:33 -0400

    More front page content

--- src/components/CardOne.js
+++ src/components/CardOne.js
@@ -6,9 +6,8 @@ function CardOne() {
         <div>
             <Card>
                 <Card.Img variant="top" src="https://images.pexels.com/photos/4050299/pexels-photo-4050299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></Card.Img>
-                <Card.Body>
-                    Welcome lets get start this is just filler text for now but I am like super glad you are here like heck yeah lets
-                    do this buddy
+                <Card.Body className="text-center">
+                    As a mentor you are not only providing the help a student may need, but you are also working on credentials for your own future. We provide the means to add to your resume a reflection of your teaching rating and history.
                     <Button href="/Signup" className="w-100 text-center mt-2">Get Started</Button>
                 </Card.Body>
             </Card>
