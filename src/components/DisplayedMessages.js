da29d22b6f981329d7d80dcee0ce467c32c8e74a
macktsawyer <macktsawyer@yahoo.com>
2021-08-31 21:02:43 -0400

    Working on inbox reading message list

---
+++ src/components/DisplayedMessages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayedMessages = () => {
+    return (
+        <div>
+            <h5>Hello</h5>
+        </div>
+    )
+}
+
+export default DisplayedMessages
--- src/components/inbox.js
+++ src/components/inbox.js
@@ -2,15 +2,38 @@ import React, { useEffect, useState } from 'react'
 import { Card, Col, Row, Navbar, Nav, Button, InputGroup, FormControl } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
 import { db } from '../firebase';
+import DisplayedMessages from './DisplayedMessages';
 
 function Inbox() {
     const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
     const [loading, setLoading] = useState(false);
-    const [timeInformation, setTimeInformation] = useState([]);
-    const [recipientInformation, setRecipientInformation] = useState([]);
-    const [messageInformation, setMessageInformation] = useState([]);
+    const [partnerUID, setPartnerUID] = useState();
+    const [partners, setPartners] = useState([]);
     const { currentUser } = useAuth();
-    const userID = currentUser.uid;
+    const userID = currentUser.uid;    
+    const fetchDisplay = async (partnerUID) => {
+        try {
+            setLoading(true);
+            let queryResults = [];
+            await sleep(5000); //Sleep needed to slow things down
+            for (let i of partnerUID) {
+                db.collection('userinfo')
+                .where("uid", "==", i)
+                .onSnapshot((querySnapshot) => {
+                    let quickSnapshot = [];
+                    querySnapshot.forEach((doc) => {
+                        quickSnapshot.push(doc.data())
+                    })
+                    queryResults.push(quickSnapshot)
+                })
+            setPartners(queryResults)
+            console.log(queryResults)
+            setLoading(false)
+            }
+        } catch (error) {
+            console.log("error: ", error);
+        }
+    };
 
     useEffect(() => {
         const fetchMessages = async () => {
@@ -20,46 +43,23 @@ function Inbox() {
                 const ref = db.collection('messages').doc(`${userID}`);
                 const docs = await ref.get();
                 let messageInfo = [];
-                let recipientInfo = [];
-                let timeInfo = [];
                 [docs].forEach((doc) => { //Promise needs an array to push fetched info
-                    let allData = doc.data();
-                    timeInfo.push(allData.messageinfo.timestamp);
-                    recipientInfo.push(allData.messageinfo.uid);
-                    messageInfo.push(allData.messageinfo.messagecontent);
+                    let messageData = doc.data();
+                    for (let i in messageData) {
+                        messageInfo.push(i)
+                    }
                 });
-                setMessageInformation(messageInfo);
-                setTimeInformation(timeInfo);
-                setRecipientInformation(recipientInfo);
-            } catch (error) {
-                console.log("error: ", error);
-            }
-        };
-        const fetchUserInfo = async () => {
-            try {
-                setLoading(true);
-                await sleep(500); //Sleep needed to slow things down
-                const ref = db.collection('messages').doc(`${userID}`);
-                const docs = await ref.get();
-                let messageInfo = [];
-                let recipientInfo = [];
-                let timeInfo = [];
-                [docs].forEach((doc) => { //Promise needs an array to push fetched info
-                    let allData = doc.data();
-                    timeInfo.push(allData.messageinfo.timestamp);
-                    recipientInfo.push(allData.messageinfo.uid);
-                    messageInfo.push(allData.messageinfo.messagecontent);
-                });
-                setMessageInformation(messageInfo);
-                setTimeInformation(timeInfo);
-                setRecipientInformation(recipientInfo);
+                setPartnerUID(messageInfo)
             } catch (error) {
                 console.log("error: ", error);
             }
         };
         fetchMessages();
+        if (partnerUID) {
+            fetchDisplay(partnerUID)
+        }
         setLoading(false); //Set loading to false in order to allow async conditional loading of info in state
-    }, [userID])
+    }, [userID, fetchDisplay, partnerUID])
 
     return (
         <div>
@@ -73,10 +73,10 @@ function Inbox() {
                 <Row>
                     <Col xs="4">
                     <Card className="text-center"><h4>Messages</h4></Card>
-                    { loading ? "Loading..." : recipientInformation.map((i) => { //Conditionally rendering and listing of profile info
+                    { loading ? "Loading..." : partners.map((i) => { //Conditionally rendering and listing of profile info
                                     return (
-                                    <Card className="w-25">
-                                        <h5>{i}</h5>
+                                    <Card className="text-center justify-content-center mt-2 h-50 w-100">
+                                        <h5 key={i.uid}> {i.displayname} </h5>
                                     </Card>
                                     )
                                 }) }
@@ -86,6 +86,7 @@ function Inbox() {
                     <h4>
                     Message Content
                     </h4>
+                    < DisplayedMessages />
                     <Card>
                     <InputGroup fixed="bottom">
                         <FormControl placeholder="Send a new message" />
