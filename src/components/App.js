00a84ef706b696579594dec0cff6293dd1eff5a9
macktsawyer <macktsawyer@yahoo.com>
2021-09-08 19:06:19 -0400

    Working on deployment

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ function App() {
     <Container className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}>
       <div className="w-100">
-        <Router basename="/front-page">
+        <Router basename="/">
           <AuthProvider>
             <Switch>
               <PrivateRoute path="/dashboard" component={Dashboard} />
